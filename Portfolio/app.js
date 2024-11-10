fd03a700dde0834c6c6fa39b77d0fc6be30e539a
nikhil_kumar <nkman2002@gmail.com>
2024-11-10 22:59:08 +0530

    internship projects

---
+++ Portfolio/app.js
@@ -0,0 +1,34 @@
+const btn = document.querySelector('.icons')
+let div = document.querySelector(".nevbarmenu")
+let active = document.querySelector(".active")
+let display = true;
+
+function toggldiv (){
+
+   if(display === true){
+        div.style.visibility = "visible";
+        display = false;
+   }else{
+    div.style.visibility = 'hidden'
+      display = true;
+   }
+   
+   
+}
+
+    
+// function hide (){
+//    div.style.visibility = 'hidden'
+// }
+
+
+btn.addEventListener("click", () => {
+    
+   toggldiv()
+    
+})
+
+div.addEventListener('click', () => {
+   div.style.visibility = 'hidden';
+   display = true
+});
